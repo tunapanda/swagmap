027e785fc0360ae4347c5605f8896aca22d61b8f
cessqt <cesswangumukima@gmail.com>
2014-10-04 15:31:56 +0300

    implemented swag item model

--- src/model/SwagItemModel.js
+++ src/model/SwagItemModel.js
@@ -1,9 +1,22 @@
+var SwagItemData = require("../data/SwagItemData");
 /**
- * SwagItemModel
+ * This is resposible for creating an item model using jsondata
  * @class SwagItemModel
  */
-function SwagItemModel() {
+function SwagItemModel(jsondata) {
+    this.x = jsondata.x;
+    this.y = jsondata.y;
+    
+}
 
+/**
+*  This gets the swagItemData from the jsondata 
+*  @method getSwagItemData
+*/
+SwagItemModel.prototype.getSwagItemData = function(){
+    var swagItemData = new SwagItemData(this.x, this.y);
+    return swagItemData;
+    
 }
 
 module.exports = SwagItemModel;
