890f7a3f73ae2d9de71f275808170262e55ffc46
cessqt <cesswangumukima@gmail.com>
2014-10-06 01:30:27 +0300

    added a load method to load items from a json file

--- src/model/SwagMapModel.js
+++ src/model/SwagMapModel.js
@@ -1,14 +1,36 @@
 var EventDispatcher = require("../utils/EventDispatcher");
 var inherits = require("inherits");
+var fs = require("fs")
 
 /**
  * Main swag map model.
  * @class SwagMapModel
  */
 function SwagMapModel() {
+    
+}
+inherits(SwagMapModel, EventDispatcher);
 
+/**
+*  This function loads the JSON file and returns a list of items 
+*  @load
+*/
+SwagMapModel.prototype.load = function(url){
+    fs.readFile(url, function (err, data){
+    	if(err){
+    		console.log("Error" + err);
+    		return;
+    	}
+    	data = JSON.parse(data);
+    	console.log(data);
+    	for (var i = 0; i < data.items.length; i++){
+	   		swagItemsDatas = new swagItemData(data[i]);
+		}
+
+    	this.trigger("loaded")
+
+    }.bind(this));
 }
 
-inherits(SwagMapModel, EventDispatcher);
 
 module.exports = SwagMapModel;
