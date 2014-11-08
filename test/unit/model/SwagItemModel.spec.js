eab812c5b05883e1221fb8e5a9e8ba93e5b59cc6
Mikael Lindqvist <li.mikael@gmail.com>
2014-11-08 12:31:33 +0100

    swag item xapi test

--- test/unit/model/SwagItemModel.spec.js
+++ test/unit/model/SwagItemModel.spec.js
@@ -1,4 +1,4 @@
-/*var SwagItemModel = require("../../../src/model/SwagItemModel");
+var SwagItemModel = require("../../../src/model/SwagItemModel");
 
 describe("SwagItemModel", function() {
 	it("can be created from data", function() {
@@ -13,4 +13,55 @@ describe("SwagItemModel", function() {
 		expect(data.getX()).toBe(20);
 		expect(data.getY()).toBe(21);
 	});
-});*/
+
+	/*it("can check if an xAPI statement matches", function() {
+		var jsonData = {
+			x: 20,
+			y: 21,
+			object: "http://example.com/activity"
+		};
+
+		var swagItemModel = new SwagItemModel(jsonData);
+
+		// At first, it should not be complete.
+		var data = swagItemModel.getSwagItemData();
+		expect(data.isComplete()).toBe(false);
+
+		// First, handle an "attempted" activity. Nothing should happen.
+		xApiStatement = {
+			"actor": {
+				"mbox": "test@example.com"
+			},
+
+			"verb": {
+				"id": "http://adlnet.gov/expapi/verbs/attempted/"
+			},
+
+			"object": {
+				"id": "http://example.com/activity"
+			}
+		};
+
+		swagItemModel.handleXApiStatement(xApiStatement);
+		expect(swagItemModel.getSwagItemData().isComplete()).toBe(false);
+
+		// Then, handle an "completed" activity.
+		// Now the data should show that the activity is completed.
+		xApiStatement = {
+			"actor": {
+				"mbox": "test@example.com"
+			},
+
+			"verb": {
+				"id": "http://adlnet.gov/expapi/verbs/completed/"
+			},
+
+			"object": {
+				"id": "http://example.com/activity"
+			}
+		};
+
+		swagItemModel.handleXApiStatement(xApiStatement);
+		expect(swagItemModel.getSwagItemData().isComplete()).toBe(true);
+	});*/
+});
